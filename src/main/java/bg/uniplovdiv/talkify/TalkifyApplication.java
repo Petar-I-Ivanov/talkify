6f7d3a0295bdc5419c270313a3edd32a212c427b
Petar-I-Ivanov <p.ivanov2955@gmail.com>
2024-12-16 20:38:04 +0200

    Added querydsl, hibernate-jcache and formatting plugin, added basic configurations, formatted code

--- src/main/java/bg/uniplovdiv/talkify/TalkifyApplication.java
+++ src/main/java/bg/uniplovdiv/talkify/TalkifyApplication.java
@@ -1,4 +1,4 @@
-package bg.uni_plovdiv.talkify;
+package bg.uniplovdiv.talkify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TalkifyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TalkifyApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TalkifyApplication.class, args);
+  }
 }

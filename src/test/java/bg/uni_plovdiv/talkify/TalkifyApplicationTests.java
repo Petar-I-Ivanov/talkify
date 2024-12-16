f65cd71d0653936954d7b693cf7cfd82389524b8
Petar-I-Ivanov <p.ivanov2955@gmail.com>
2024-12-16 19:27:27 +0200

    First commit

---
+++ src/test/java/bg/uni_plovdiv/talkify/TalkifyApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.uni_plovdiv.talkify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
